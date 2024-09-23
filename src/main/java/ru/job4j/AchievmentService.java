0894c0b64a7a093532df41c4986fa499fe74a4a0
Иван <ivan1990kalinin@yandex.ru>
2024-09-23 19:53:55 +0700

    Архитектура проекта " Бот-Помощник по настроению" [#505287]

---
+++ src/main/java/ru/job4j/AchievmentService.java
@@ -0,0 +1,6 @@
+package ru.job4j;
+
+public class AchievmentService {
+    private TelegramBotService telegramBotService;
+    private Repositories repositories;
+}
---
+++ src/main/java/ru/job4j/ReminderService.java
@@ -0,0 +1,6 @@
+package ru.job4j;
+
+public class ReminderService {
+    private TelegramBotService telegramBotService;
+    private Repositories repositories;
+}
