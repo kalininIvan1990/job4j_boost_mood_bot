f3add607cd59403df2ce869ef05bab92f89fd3c7
Иван <ivan1990kalinin@yandex.ru>
2024-09-29 18:36:35 +0700

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/BotCommandHandler.java
+++
@@ -1,5 +0,0 @@
-package ru.job4j;
-
-public class BotCommandHandler {
-    private MoodService moodService;
-}
--- src/main/java/ru/job4j/TelegramBotService.java
+++
@@ -1,5 +0,0 @@
-package ru.job4j;
-
-public class TelegramBotService {
-    private BotCommandHandler botCommandHandler;
-}
